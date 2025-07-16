530d28c82cbf45173f8d3c6c0337b6accdfeebfc
DavidPrevosto <dprevosto@cordobasoftware.com.ar>
2025-07-16 15:20:47 -0300

    Agregando estilos globales, primer maquetado, definiendo secciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
+import styled from "styled-components"
+import { GlobalStyles, MyRoutes, Sidebar } from "./index"
+import { Device } from "./styles/breakpoints"
 
 function App() {
   return (
     <>
-      <span>Hola</span>
+     <Container>
+      <GlobalStyles />
+      <section className="contentSidebar" ><Sidebar /></section>
+      <section className="contentMenuambur" >Menu ambur</section>
+      <section className="contentRouters" >{<MyRoutes />}</section>
+
+     </Container>
     </>
   )
 }
+
+const Container = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  background: black;
+  .contentSidebar {
+    display: none;
+    background-color: rgb(78,45,71,0.5)
+  }
+  .contentMenuambur {
+    position: absolute;
+    background-color: rgb(13,241,215,0.5)
+  }
+  .contentRouters {
+    background-color: rgb(231,13,136,0.5);
+    grid-column: 1;
+    width: 100%;
+  }
+  @media ${Device.tablet} {
+    grid-template-columns: 88px 1fr;
+    .contentSidebar {
+      display: initial;
+    }
+    .contentMenuambur {
+      display: none;
+    }
+    .contentRouters {
+      grid-column: 2;
+    }
+  }
+`;
 
 export default App
--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -1,1 +1,15 @@
 import { createGlobalStyle} from "styled-components"
+
+export const GlobalStyles = createGlobalStyle`
+   
+
+    body {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        background-color: black;
+        font-family: "Poppins", sans-serif;
+        color: #fff;
+    }
+
+`
