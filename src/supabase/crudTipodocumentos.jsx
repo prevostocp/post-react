a9cab3fcffe7a37956215e5763c40ea7c37dd9db
DavidPrevosto <dprevosto@cordobasoftware.com.ar>
2025-07-29 16:11:25 -0300

    correcciones insrtar usuario

--- src/supabase/crudTipodocumentos.jsx
+++ src/supabase/crudTipodocumentos.jsx
@@ -1,8 +1,17 @@
 import { supabase } from "../index";
+import Swal from "sweetalert2"
 
 const tabla = "tipodocumento";
 export async function MostrarTipoDocumentos(id_empresa) {
-    const { data } = await supabase.from(tabla)
+    const { data, error } = await supabase.from(tabla)
         .select().eq("id_empresa", id_empresa);
+    if (error) {
+            // Swal.fire({
+            //     icon: "error",
+            //     title: "Oops... Tipo doc",
+            //     text: error.message
+            // });
+            // return;
+    }
     return data;
 }
